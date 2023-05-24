1e621fec44c877def6e11457a2444b77c57e7945
dynisty18 <43758353+dynisty18@users.noreply.github.com>
2023-05-24 15:04:31 -0400

    Add files via upload

---
+++ TAD processing script.R
@@ -0,0 +1,37 @@
+##Load library
+library(tidyverse)
+
+#Load Data
+raw_data <- read_csv("Dynisty_rule_data.csv")
+raw_data <- X2022_11_04_Dpse_UCI_mRNA_Dpse_TL_SPE123_6_3_B_Flye_v2_pilon_mb_hits
+
+#group by Query and subject and choose the highest percentage
+decision_data <- raw_data %>% group_by(Query,Subject) %>% arrange(desc(`% Match`)) %>%
+  mutate(choice = if_else(row_number() == 1,"yes","no" ))
+
+a <- decision_data %>% filter(Subject == "Dpseu_CH__TAD_Boundary_451_452_03221282_03226013")
+#Check to make sure it is correct 
+a <- decision_data4 %>% filter(Query == "XM_001352246.4")
+
+write.csv(decision_data, file="high_match.csv")
+
+
+
+
+#Take Query and Locus from file 1 and match to file 2. When we find the match, we keep TAD_ID
+setwd("C:/Users/dynis/OneDrive - The Pennsylvania State University/R data and scripts")
+library(readxl)
+Blast_data <- read_excel("Blast_of_AR_to_TL.xlsx")
+dpse_data <- read_excel("2023_05_13_Dpse_UCI_All_RNA.xlsx",sheet = 3)
+
+join_data <- inner_join(Blast_data,dpse_data[,c("Transcript","Locus","TAD_ID")],by = c("Query" = "Transcript",
+                                                                                       "Locus" = "Locus"))
+
+#Add include1 column to remove redundant loci
+Blast <- AR_to_TL %>%
+  group_by(Locus, Subject) %>%
+  mutate(Include1 = if_else(duplicated(Locus)==FALSE, "yes", "no"))
+
+write.csv(Blast, file = "Blast of AR to TL.csv")
+
+
